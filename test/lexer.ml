bf1ef6e5e6090fa736068dab2fea4c0aa67c5c82
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-12-23 14:42:16 +1100

    just work on token types instead of tokens to avoid locations etc in tests

--- test/lexer.ml
+++ test/lexer.ml
@@ -2,21 +2,25 @@
 open Paper
 
 let pprint_token ppf = function
-  | Lexer.LeftParen -> Fmt.pf ppf "LeftParen "
+  | Lexer.LeftParen -> Fmt.pf ppf "LeftParen"
+  | Lexer.RightParen -> Fmt.pf ppf "RightParen"
+  | Lexer.Number -> Fmt.pf ppf "Number"
   | _ -> Fmt.pf ppf ""
 
 let token_eq a b = a = b
 let token_testable = Alcotest.testable pprint_token token_eq
 
+let token_types_only tokens = let open Lexer in List.map (fun t -> t.token_type) tokens
+
 let test_single_int_literal () =
-  let ts = Lexer.tokenise "(" in
-  let t = List.nth ts 0 in
-  let ttype = t.token_type in
-  Alcotest.(check token_testable) "left paren" LeftParen ttype
+  let ts = Lexer.tokenise "5\n" in
+  let tts = token_types_only ts in
+  
+  Alcotest.(check (list token_testable)) "Number" [Number] tts
 
 (* Run it *)
 let () =
   let open Alcotest in
   run "Tests" [
-      "basic tokenise", [ test_case "Left parenthesis" `Quick test_single_int_literal  ];
+      "basic tokenise", [ test_case "Single int literal" `Quick test_single_int_literal ];
     ]

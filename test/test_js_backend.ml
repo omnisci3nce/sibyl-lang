eb66c734051c81de703255a3de9c16a0d9d0e36d
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-07-23 20:23:38 +1000

    add constant folding to the JS tests

--- test/test_js_backend.ml
+++ test/test_js_backend.ml
@@ -7,26 +7,12 @@ let run cmd =
 
 let run_source_and_collect_output source =
   let gen = Backends.JS.new_generator "test.js" in
-  let instr = source |> Lexer.tokenise |> Parser.parse |> Backends.JS.codegen gen in
+  let instr = source |> Lexer.tokenise |> Parser.parse |> Optimise.constant_fold |> Backends.JS.codegen gen in
   let ch = open_out "test.js" in
   Printf.fprintf ch "%s" instr; flush ch; close_out ch;
   (* let return_code =  Sys.command "node ./test.js" in *)
   let sss = run "node ./test.js" in
   (instr, sss)
-
-let fibonacci n = Printf.sprintf "
-fn fib(n) {
-  let a = 0
-  let next1 = n - 1
-  let next2 = n - 2
-  if (n < 2)
-    return n
-  else
-    return (fib(next1) + fib(next2))
-}
-let a = fib(%d)
-print a
-" n
 
 let fibonacci_new = Printf.sprintf "
 fn fib(n) {
@@ -50,11 +36,22 @@ let test_variable () =
   let expected = "const a = 5\nconsole.log(a)\n" in
   Alcotest.(check string) "declares variable" expected src
 
+let test_addition () =
+  let (src, output) = run_source_and_collect_output "
+  let a = 5 + 5
+  print a
+  " in
+  let expected = "const a = 10\nconsole.log(a)\n" in
+  Alcotest.(check string) "js is readable" expected src;
+  Alcotest.(check string) "output is correct" "10" output
+
+
 let () =
   let open Alcotest in
   run "Tests" [
       "function", [ 
         (* test_case "fibonacci function" `Quick test_fibonacci; *)
-        test_case "variable declaration" `Quick test_variable
+        test_case "variable declaration" `Quick test_variable;
+        test_case "addition" `Quick test_addition
      ]
   ]
